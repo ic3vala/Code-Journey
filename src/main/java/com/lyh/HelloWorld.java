ab6ef6102c0cb9aa56a776dd5aa8e94a266a76e5
ic3vala <liuyuheng0035@naver.com>
2025-02-15 14:53:03 +0900

    第一次尝试同步

--- src/main/java/com/lyh/HelloWorld.java
+++ src/main/java/com/lyh/HelloWorld.java
@@ -2,6 +2,6 @@ package com.lyh;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("CodeJourney Day1 启动成功！");
+        System.out.println("Hello World！");
     }
 }
